5fd1390b695dce840dd3f71e4d6915c057af6b8f
Sergio Nuñez Vega <sergio.i.legion@gmail.com>
2017-03-06 12:34:14 +0100

    yeoman setup
    fresh yeoman setup using the angular generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leagueChampionsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leagueChampionsApp
+ */
+angular.module('leagueChampionsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leagueChampionsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leagueChampionsApp
+ */
+angular.module('leagueChampionsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
